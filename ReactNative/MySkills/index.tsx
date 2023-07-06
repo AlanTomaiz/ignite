ddf820b4995360451a7450bc1df8c059f1ab55da
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-07-06 10:54:58 -0300

    Delete ReactNative/MySkills directory

--- ReactNative/MySkills/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import {Home} from './src/pages/Home';
-
-AppRegistry.registerComponent(appName, () => Home);
