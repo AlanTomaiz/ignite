2946c7fba04998f7bcf2032043c96defef120d25
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-07-12 21:52:21 -0300

    faunadb

--- front/ignews/src/pages/api/auth/[...nextauth].ts
+++ front/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query } from 'faunadb';
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export const authOptions = {
   providers: [
@@ -8,6 +10,48 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { name, email } = user;
+
+      try {
+        // await fauna.query(
+        //   query.Create(
+        //     query.Collection('users'),
+        //     { data: { name, email } }
+        //   )
+        // );
+
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email),
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { name, email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(email),
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+
+    }
+  }
 }
 
 export default NextAuth(authOptions)
