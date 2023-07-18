87c15ee05f287bdb49076ff8c592b8a1349fe272
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-07-17 23:35:10 -0300

    configuração chrakra ui e fonte

--- front/dashgo/src/pages/_document.tsx
+++ front/dashgo/src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
