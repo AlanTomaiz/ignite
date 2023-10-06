fb59bd11c77933645ba37f9b58385854d6a6725b
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-10-06 18:24:45 -0300

    feat: product box

--- front/ignite-shop/src/pages/index.tsx
+++ front/ignite-shop/src/pages/index.tsx
@@ -1,5 +1,30 @@
+import Image from 'next/image'
+
+import { HomeContainer, ProductBox } from "../styles/(pages)/home"
+
+import camiseta1 from '../assets/camisetas/t-shirt-1.png'
+import camiseta2 from '../assets/camisetas/t-shirt-2.png'
+import camiseta3 from '../assets/camisetas/t-shirt-3.png'
+import camiseta4 from '../assets/camisetas/t-shirt-4.png'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <HomeContainer>
+      <ProductBox>
+        <Image src={camiseta1} width={520} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>$79.90</span>
+        </footer>
+      </ProductBox>
+
+      <ProductBox>
+        <Image src={camiseta2} width={520} alt="" />
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>$79.90</span>
+        </footer>
+      </ProductBox>
+    </HomeContainer>
   )
 }
