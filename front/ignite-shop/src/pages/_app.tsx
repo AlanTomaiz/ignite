dcae7122933c30fd6444a9648932559d62b1bbf8
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-10-06 17:25:18 -0300

    feat: project header

--- front/ignite-shop/src/pages/_app.tsx
+++ front/ignite-shop/src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from "next/app"
+import Image from 'next/image'
+
 import { globalStyles } from "../styles/globa"
+import { Container, Header } from "../styles/(pages)/app"
+
+import logo from '../assets/logo.svg'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo} alt="Ignite Shop" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
