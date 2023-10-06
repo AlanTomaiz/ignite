724bd24026c068553e717e03e28c337aa5b30e86
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-10-06 17:03:18 -0300

    feat: global styles

--- front/ignite-shop/pages/_app.tsx
+++ front/ignite-shop/pages/_app.tsx
@@ -1,5 +1,8 @@
-function MyApp({ Component, pageProps }) {
+import { AppProps } from "next/app"
+import { globalStyles } from "./styles/globa"
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
