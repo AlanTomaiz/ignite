1ad1d9d8a39f091d355e6c0561956f07830fddf8
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-10-09 15:43:53 -0300

    feat: create product carrocel

--- front/ignite-shop/src/pages/index.tsx
+++ front/ignite-shop/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import { HomeContainer, ProductBox } from "../styles/(pages)/home"
 
@@ -8,9 +10,16 @@ import camiseta3 from '../assets/camisetas/t-shirt-3.png'
 import camiseta4 from '../assets/camisetas/t-shirt-4.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <ProductBox>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <ProductBox className="keen-slider__slide">
         <Image src={camiseta1} width={520} alt="" />
         <footer>
           <strong>Camiseta X</strong>
@@ -18,10 +27,26 @@ export default function Home() {
         </footer>
       </ProductBox>
 
-      <ProductBox>
+      <ProductBox className="keen-slider__slide">
         <Image src={camiseta2} width={520} alt="" />
         <footer>
           <strong>Camiseta Y</strong>
+          <span>$79.90</span>
+        </footer>
+      </ProductBox>
+
+      <ProductBox className="keen-slider__slide">
+        <Image src={camiseta3} width={520} alt="" />
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>$79.90</span>
+        </footer>
+      </ProductBox>
+
+      <ProductBox className="keen-slider__slide">
+        <Image src={camiseta4} width={520} alt="" />
+        <footer>
+          <strong>Camiseta Z</strong>
           <span>$79.90</span>
         </footer>
       </ProductBox>
