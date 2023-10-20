91cd478582e4fbaed4529604512ee5adb60f7553
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2023-10-20 01:43:26 -0300

    feat: next auth adapter

---
+++ front/ignite-call/src/@types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import 'next-auth'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    name: string
+    email: string | undefined
+    username: string
+    avatar_url: string | undefined
+  }
+}
